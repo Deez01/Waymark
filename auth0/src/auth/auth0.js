069b8632a3ec5dc399df25f95d23a571fde1dbd8
Deez01 <bryan03estrada31@gmail.com>
2025-12-12 23:07:50 -0800

    Configure Auth0 client.

---
+++ auth0/src/auth/auth0.js
@@ -0,0 +1,11 @@
+// Name:Bryan Estrada-Cordoba 
+
+// Auth0 Config file
+
+import Auth0 from 'react-native-auth0';
+import { AUTH0_DOMAIN, AUTH0_CLIENT_ID } from '@env';
+
+export const auth0 = new Auth0 ({
+	domain: AUTH0_DOMAIN, 
+	clientId: AUTH0_CLIENT_ID,
+});
