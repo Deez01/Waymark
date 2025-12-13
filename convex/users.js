68c9a204fb213e15b5e02e654074a24d568f8c6b
Deez01 <bryan03estrada31@gmail.com>
2025-12-12 23:49:36 -0800

    Updated code.

--- convex/users.js
+++ convex/users.js
@@ -1,6 +1,7 @@
 // Name: Bryan Estrada-Cordoba
 // convex/users.js
-import { mutation, query } from "./_generated/server";
+import { mutation } from "./_generated/server";
+import { v } from "convex/values";
 
 export const createUser = mutation({
   args: {
@@ -17,5 +18,32 @@ export const createUser = mutation({
     if (!existing) {
       await ctx.db.insert("users", args);
     }
+  },
+});
+
+export const updateUser = mutation({
+  args: {
+    auth0Id: v.string(),
+    name: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_auth0Id", q => q.eq("auth0Id", args.auth0Id))
+      .first();
+
+    await ctx.db.patch(user._id, { name: args.name });
+  },
+});
+
+export const deleteUser = mutation({
+  args: { auth0Id: v.string() },
+  handler: async (ctx, args) => {
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_auth0Id", q => q.eq("auth0Id", args.auth0Id))
+      .first();
+
+    await ctx.db.delete(user._id);
   },
 });
