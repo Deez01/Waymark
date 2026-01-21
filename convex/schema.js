738753e6fbd90329d3fb03ddcca3dba3088c2f94
Painted-J <jacobopineda1999@gmail.com>
2026-01-21 10:46:47 -0800

    Map view functionality fix, using react-native-maps

--- convex/schema.js
+++ convex/schema.js
@@ -1,12 +1,13 @@
 // Name: Bryan Estrada-Cordoba
 // convex/schema.js
-
 import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values"; // 1. Import 'v'
 
 export default defineSchema({
   users: defineTable({
-    auth0Id: "string",
-    email: "string",
-    name: "string",
+    // 2. Use v.string() instead of "string"
+    auth0Id: v.string(),
+    email: v.string(),
+    name: v.string(),
   }).index("by_auth0Id", ["auth0Id"]),
 });
--- convex/src/client.js
+++
@@ -1,7 +0,0 @@
-// Name: Bryan Estrada-Cordoba
-
-import { ConvexReactClient } from "convex/react";
-
-export const convex = new ConvexReactClient(
-  process.env.EXPO_PUBLIC_CONVEX_URL
-);
